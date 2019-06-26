e6cbfc9e6562e23bc2bc25dd174ec39ce2e72983
luhaojian11 <962671114@qq.com>
2019-06-26 22:43:01 +0800

    Initial commit

---
+++ src/model/Main.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student(20150001,"John",20,true);
+        System.out.println(student.toString());
+    }
+}
